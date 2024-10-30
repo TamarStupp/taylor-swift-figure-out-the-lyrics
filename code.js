a39edeac9a3ee24ad85dca1386f254c198f58723
tamarStupp <tamar.stupp@gmail.com>
2024-10-30 09:31:24 +0200

    add restart button

--- code.js
+++ code.js
@@ -4,8 +4,10 @@ let minutesLeft = 10;
 let secondsLeft = 13;
 let timerInterval;
 let isPaused = false;
-// songs without TTPD: 199
-const NUMBER_OF_SONGS = 231;
+const restartIcon = `<svg viewBox="0 0 31 28" fill="none" xmlns="http://www.w3.org/2000/svg" class="restart-icon">
+<path d="M10.6962 22.6168C10.0167 22.1429 9.08169 22.3096 8.60777 22.989C8.13385 23.6685 8.3005 24.6035 8.97998 25.0775L10.6962 22.6168ZM4.49195 13.2169L3.12185 13.8275C3.29985 14.2269 3.64328 14.5287 4.06222 14.654C4.48117 14.7793 4.93393 14.7155 5.30196 14.4794L4.49195 13.2169ZM9.25288 11.9445C9.95013 11.4971 10.1527 10.5692 9.70536 9.87197C9.258 9.17472 8.33011 8.97213 7.63285 9.41949L9.25288 11.9445ZM3.62352 7.58326C3.2863 6.82657 2.39952 6.48653 1.64283 6.82375C0.886143 7.16097 0.546099 8.04776 0.883319 8.80444L3.62352 7.58326ZM7.82475 7.81245C11.1207 3.0868 17.7752 1.95092 22.7096 5.3925L24.4258 2.93188C18.1947 -1.41411 9.6508 -0.0498022 5.36413 6.09625L7.82475 7.81245ZM22.7096 5.3925C27.644 8.83407 28.877 15.4712 25.581 20.1969L28.0417 21.9131C32.3283 15.767 30.6569 7.27786 24.4258 2.93188L22.7096 5.3925ZM25.581 20.1969C22.285 24.9225 15.6306 26.0584 10.6962 22.6168L8.97998 25.0775C15.2111 29.4234 23.755 28.0591 28.0417 21.9131L25.581 20.1969ZM5.99078 13.2762C6.06623 11.3694 6.66341 9.47753 7.82475 7.81245L5.36413 6.09625C3.86274 8.24888 3.0904 10.6993 2.99313 13.1576L5.99078 13.2762ZM7.63285 9.41949L3.68194 11.9544L5.30196 14.4794L9.25288 11.9445L7.63285 9.41949ZM0.883319 8.80444L3.12185 13.8275L5.86206 12.6063L3.62352 7.58326L0.883319 8.80444Z" fill="var(--bold-text)"/>
+</svg>
+`
 
 /**
  * @param {Array} chosenAlbums deafult: empty arry. The names of 
@@ -14,8 +16,13 @@ const NUMBER_OF_SONGS = 231;
 const getRandomSongLyrics = async () => {
     let response = await fetch(`./songs/allSongs/randomSong`);
     // if retriving the song lyrics after five tries is impossible, show error message
-    if (!response.ok ) {
-        customAlert(document.querySelector('#no-song'));
+    if (!response.ok) {
+        console.error(`fetch failed. Response status: ${response.status}`);
+        if (navigator.onLine) {
+            customAlert(document.querySelector('#no-song'));
+        } else {
+            customAlert(document.querySelector('#no-internet'));
+        }
     }
     const result = await response.text();
     lyrics = result.split(/[ (/\n)]/g);
@@ -34,16 +41,17 @@ const getRandomSongLyrics = async () => {
 
     // pause the game when user changes tabs
     document.addEventListener("visibilitychange", changeVisibility);
-    
+
 
     // activate timer
+    console.log(timerInterval);
     timerInterval = setInterval(updateTimer, 1000);
     isPaused = false;
 }
 
 const changeVisibility = (e) => {
     if (document.visibilityState !== "visible") {
-         document.getElementById("pause-btn").click();
+        document.getElementById("pause-btn").click();
     }
 }
 
@@ -64,8 +72,8 @@ const registerEntrance = async () => {
                 mode: 'no-cors',
                 "method": "POST"
             }
-            );
-            console.log('registered');
+        );
+        console.log('registered');
     }
 }
 
@@ -86,8 +94,8 @@ const createWordElements = () => {
         wordDiv = document.createElement('div');
         wordDiv.setAttribute('data-word', sterileWord);
         wordDiv.classList.add('word');
-        wordDiv.innerText= lyrics[index];
-        document.getElementById('lyrics-container').appendChild(wordDiv);
+        wordDiv.innerText = lyrics[index];
+        document.getElementById('lyrics').appendChild(wordDiv);
         // Adds sterile words to uniqueLyrics
         if (!uniqueLyrics.includes(sterileWord)) {
             uniqueLyrics.push(sterileWord);
@@ -101,10 +109,10 @@ const sterilize = (value) => {
     let extractLettersRegex = /[a-z0-9]*/gi;
     let returnValue = value.match(extractLettersRegex).join('').toLowerCase();
     // check for exclamation word - converting to Set to remove duplicates
-    let exclamations = [... new Set(returnValue.match(/^ah|^oh|^yeah|^uh|^ooh|^oo/g))].join(''); 
+    let exclamations = [... new Set(returnValue.match(/^ah|^oh|^yeah|^uh|^ooh|^oo/g))].join('');
     // change ooh to oh because it's confusing
-    if (exclamations === 'ooh') { exclamations = 'oh' } 
-    return (exclamations || returnValue); 
+    if (exclamations === 'ooh') { exclamations = 'oh' }
+    return (exclamations || returnValue);
 }
 
 const onInput = (event) => {
@@ -164,16 +172,16 @@ const finishGame = () => {
     document.getElementById('timer').classList.remove('end-of-time');
     document.getElementById('give-up').removeEventListener('click', finishGame);
     // document.getElementById('give-up').style.display = "none";
-    document.getElementById('give-up').innerText= "Restart";
+    document.getElementById('give-up').innerHTML = `<span>Restart</span>${restartIcon}`;
     document.getElementById('give-up').addEventListener('click', restart);
 
     clearInterval(timerInterval);
     document.getElementById('guess').readOnly = true;
     document.getElementById('guess').classList.add('endGame');
-    document.getElementById('guess').setAttribute('placeholder',"Game's Over!");
+    document.getElementById('guess').setAttribute('placeholder', "Game's Over!");
     document.getElementById('guess').value = '';
-    document.querySelectorAll('.word:not(.black)').forEach(el => {el.classList.add('red')});
-    document.getElementById('precent').innerText = `Success: ${Math.round(document.getElementsByClassName('black').length/lyrics.length * 100)}%`;
+    document.querySelectorAll('.word:not(.black)').forEach(el => { el.classList.add('red') });
+    document.getElementById('precent').innerText = `Success: ${Math.round(document.getElementsByClassName('black').length / lyrics.length * 100)}%`;
     document.querySelector('.score').classList.remove('hidden');
     document.querySelector('.score').style.pointerEvents = 'none';
     // customAlert("Time's up", document.querySelector(".black-screen"));
@@ -184,35 +192,51 @@ const finishGame = () => {
 const customAlert = (wrapper) => {
     if (!isPaused) {
         isPaused = true;
+        console.log(timerInterval);
         clearInterval(timerInterval);
         // wrapper.querySelector('.text').innerText = text;
         let promise = new Promise((resolve, reject) => {
-            wrapper.addEventListener('click', (event) => {
-                if (event.target.classList.contains('close-btn')) {
-                    wrapper.style.display = 'none';
-                    resolve("next");
+            wrapper.addEventListener('click', function onClickWrapper (event) {
+                const classList = event.target.classList;
+                if (classList.contains('close-btn')) {
+                    resolve(true);
+                } else if (classList.contains('start-over')) {
+                    resolve(false);
+                    restart();
                 }
+                event.target.removeEventListener("click", onClickWrapper);
             });
         });
-        promise.then(() => {
-            timerInterval = setInterval(updateTimer, 1000);
+        promise.then((isStartTimer) => {
+            wrapper.style.display = 'none';
+            if (isStartTimer) {timerInterval = setInterval(updateTimer, 1000);}
             isPaused = false;
         })
         wrapper.style.display = 'block';
-        return(promise);
+        return (promise);
     }
 }
 
+
 /*-------------- restart ------------------ */
 const restart = () => {
-    lyrics;
+    lyrics = [];
     uniqueLyrics = [];
     minutesLeft = 10;
     secondsLeft = 13;
-    timerInterval;
+    console.log('clearInterval restart');
+    clearInterval(timerInterval);
+    timerInterval = '';
     isPaused = false;
-    document.getElementById('give-up').innerText= "give up";
+    document.getElementById('lyrics').innerHTML = '';
+    document.getElementById('give-up').innerText = "give up";
+    document.getElementById('precent').innerText = '';
     document.getElementById('give-up').removeEventListener('click', restart);
+    document.querySelector('.timer').innerText = `${String(minutesLeft).padStart(2, '0')}:${String(secondsLeft).padStart(2, '0')}`;
+    document.getElementById('guess').readOnly = false;
+    document.getElementById('guess').classList.remove('endGame');
+    document.querySelector('.score').style.pointerEvents = 'all';
+    document.getElementById('pause-btn').style.pointerEvents = 'all';
     getRandomSongLyrics();
 }
 
@@ -220,15 +244,6 @@ const restart = () => {
 navigator.serviceWorker.register('./serviceWorker.js').then((registration) => {
     console.log('service worker registered');
 });
-
-
-// navigator.serviceWorker.ready.then(registration => {
-//     console.log('controller: ', navigator.serviceWorker.controller);
-//   });
-
-// navigator.serviceWorker.addEventListener('controllerchange', () => {
-//     console.log('new service worker took control')
-// })
 
 // start the program
 getRandomSongLyrics();
